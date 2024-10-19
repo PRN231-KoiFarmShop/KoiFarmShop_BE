214fc65f35600c03f1a445b0ac35434fa559a2a4
trankien214 <kientse171441@fpt.edu.vn>
2024-10-19 10:30:07 +0700

    Manage User 1.0

---
+++ src/ks.application/Models/Users/UserUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ks.application.Models.Users
+{
+    public class UserUpdateModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
---
+++ src/ks.application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,28 @@
+﻿using ks.application.Models.Fish;
+using ks.application.Models.Users;
+using ks.application.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ks.application.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserViewModel?> CreateUserAsync(UserCreateModel model,
+    CancellationToken cancellationToken = default);
+        Task<PaginatedList<UserViewModel>?> GetAsync(int? pageSize,
+            string search = "",
+            int pageIndex = 0,
+            CancellationToken cancellationToken = default);
+        Task<bool> RemoveAsync(Guid id,
+            CancellationToken cancellationToken = default);
+        Task<UserViewModel?> GetById(Guid id,
+            CancellationToken cancellationToken = default);
+        Task<bool> UpdateAsync(Guid id,
+            UserUpdateModel model,
+            CancellationToken cancellationToken = default);
+    }
+}
