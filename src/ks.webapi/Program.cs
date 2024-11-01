d279873bc5e794337ad2ec79693e28575a82271e
Nguyen Thai Trung Kien <kiennttse171282@fpt.edu.vn>
2024-11-02 03:04:59 +0700

    add to cart function using session cookies to save in local

--- src/ks.webapi/Program.cs
+++ src/ks.webapi/Program.cs
@@ -41,7 +41,6 @@ void ApplyMigrations()
     var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     if (_db.Database.GetPendingMigrations().Count() > 0)
     {
-
         _db.Database.Migrate();
     }
 }
