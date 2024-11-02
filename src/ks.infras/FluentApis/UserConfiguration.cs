2122aa142fa792b491bbba0618ab5474bee13baf
trankien214 <kientse171441@fpt.edu.vn>
2024-11-02 20:58:58 +0700

    Remove PhoneNumber is Unique

--- src/ks.infras/FluentApis/UserConfiguration.cs
+++ src/ks.infras/FluentApis/UserConfiguration.cs
@@ -9,8 +9,8 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.HasKey(x => x.Id);
-        builder.HasIndex(x => x.PhoneNumber)
-            .IsUnique(true);
+        builder.HasIndex(x => x.PhoneNumber);
+            /*.IsUnique(true);*/
         builder.HasIndex(x => x.Email)
             .IsUnique(true);
         builder.HasMany(x => x.Orders)
